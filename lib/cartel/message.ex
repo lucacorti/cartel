ef54fa56afa7fec7705edc677e3f81a81d49bed7
Luca Corti <luca@fantacast.it>
2016-01-28 00:36:39 +0100

    Use @callback instead of use Behavior

--- lib/cartel/message.ex
+++ lib/cartel/message.ex
@@ -1,6 +1,4 @@
 defmodule Cartel.Message do
-  use Behaviour
-
-  defcallback encode(message :: Cartel.Pusher.Message.t) :: binary
-  defcallback decode(binary :: binary) :: Cartel.Pusher.Message.t
+  @callback encode(message :: Cartel.Pusher.Message.t) :: binary
+  @callback decode(binary :: binary) :: Cartel.Pusher.Message.t
 end
