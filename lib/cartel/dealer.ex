7c9810b64c713d3ba15ead53d2b3849f56bd3ca7
Luca Corti <luca@fantacast.it>
2016-01-30 11:32:11 +0100

    Also make func for dealer name

--- lib/cartel/dealer.ex
+++ lib/cartel/dealer.ex
@@ -4,18 +4,18 @@ defmodule Cartel.Dealer do
   alias Cartel.Pusher.{Apns, Gcm}
 
   def start_link(args) do
-    GenServer.start_link(__MODULE__, args, name: :"Cartel.Dealer@#{args[:id]}")
+    GenServer.start_link(__MODULE__, args, name: dealer_name(args[:id]))
   end
 
   def send(appid, type, message) do
-    GenServer.call(:"Cartel.Dealer@#{appid}", {:send, appid, type, message})
+    GenServer.call(dealer_name(appid), {:send, appid, type, message})
   end
 
   def init(args) do
     import Supervisor.Spec, warn: false
     children = args[:pushers]
     |> Enum.map(fn pusher ->
-      id = make_name(args[:id], pusher[:type])
+      id = pusher_name(args[:id], pusher[:type])
       case pusher[:type] do
         :apns -> worker(Apns, [id, pusher], id: id)
         :gcm -> worker(Gcm, [id, pusher], id: id)
@@ -26,16 +26,20 @@ defmodule Cartel.Dealer do
   end
 
   def handle_call({:send, appid, type = :apns, message}, _from, state) do
-    pid = GenServer.whereis(make_name(appid, type))
+    pid = GenServer.whereis(pusher_name(appid, type))
     {:reply, Apns.send(pid, message), state}
   end
 
   def handle_call({:send, appid, type = :gcm, message}, _from, state) do
-    pid = GenServer.whereis(make_name(appid, type))
+    pid = GenServer.whereis(pusher_name(appid, type))
     {:reply, Gcm.send(pid, message), state}
   end
 
-  defp make_name(id, type) do
+  defp dealer_name(id) do
+    :"Cartel.Dealer@#{id}"
+  end
+
+  defp pusher_name(id, type) do
     :"Cartel.Pusher@#{id}/#{type}"
   end
 end
