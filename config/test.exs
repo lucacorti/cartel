6a7fb930b4fd5fc922b48073d93acc75011e45e5
Luca Corti <luca@fantacast.it>
2016-03-05 16:02:37 +0100

    remove test config

--- config/test.exs
+++ config/test.exs
@@ -4,42 +4,4 @@ alias Cartel.Pusher.{Apns,Gcm}
 
 config :logger, level: :debug
 
-config :cartel, dealers: [
-  [
-    id: "com.mydomain1.myapp1",
-    pushers: [
-      [
-        type: Apns,
-        env: :sandbox,
-        cert: "/Users/luca/Desktop/keys/tsc-apns-dev-crt.pem",
-        key: "/Users/luca/Desktop/keys/tsc-apns-dev-key.pem",
-        cacert: "/Users/luca/Desktop/keys/entrust_2048_ca.cer"
-      ],
-      [
-        type: Gcm,
-        key: "abc"
-      ]
-    ]
-  ],
-  [
-    id: "com.mydomain1.myapp2",
-    pushers: [
-      [
-        type: Apns,
-        env: :sandbox,
-        cert: "/Users/luca/Desktop/keys/tsc-apns-dev-crt.pem",
-        key: "/Users/luca/Desktop/keys/tsc-apns-dev-key.pem",
-        cacert: "/Users/luca/Desktop/keys/entrust_2048_ca.cer"
-      ]
-    ]
-  ],
-  [
-    id: "com.mydomain2.myapp1",
-    pushers: [
-      [
-        type: Gcm,
-        key: "abc"
-      ]
-    ]
-  ]
-]
+config :cartel, dealers: []
