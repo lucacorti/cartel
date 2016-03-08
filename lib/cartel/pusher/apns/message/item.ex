dcac33070a5dd1a6a5ac1fc7da25f50f3f408e58
Luca Corti <luca@fantacast.it>
2016-03-08 22:41:33 +0100

    Use pattern matching for Item encoding

--- lib/cartel/pusher/apns/message/item.ex
+++ lib/cartel/pusher/apns/message/item.ex
@@ -1,5 +1,6 @@
 defmodule Cartel.Pusher.Apns.Message.Item do
   defstruct [:id, :data]
+  alias Cartel.Pusher.Apns.Message.Item
 
   @device_token 1
   def device_token, do: @device_token
@@ -22,29 +23,34 @@ defmodule Cartel.Pusher.Apns.Message.Item do
   @priority_when_convenient 5
   def priority_when_convenient, do: @priority_when_convenient
 
-  def encode(item) do
-    case item.id do
-      @device_token ->
-        {:ok, token} = Base.decode16(item.data, case: :mixed)
-        {:ok, <<item.id::size(8), 32::size(16)>> <> token}
-      @payload ->
-        {:ok, msg_payload} = Poison.encode(item.data)
-        payload_size = byte_size(msg_payload)
-        {:ok, <<item.id::size(8), payload_size::size(16)>> <> msg_payload}
-      @notification_identifier ->
-        {:ok, <<item.id::size(8), 4::size(16), item.data::size(32)>>}
-      @expiration_date ->
-        date = item.data
-        if date == nil do
-          date = 0
-        end
-        {:ok, <<item.id::size(8), 4::size(16), expiration_date::size(32)>>}
-      @priority ->
-        msg_priority = item.data
-        if msg_priority == nil do
-          msg_priority = @priority_immediately
-        end
-        {:ok, <<item.id::size(8), 1::size(16), msg_priority::size(32)>>}
+  def encode(item = %Item{id: @device_token}) do
+    {:ok, token} = Base.decode16(item.data, case: :mixed)
+    {:ok, <<item.id::size(8), 32::size(16)>> <> token}
+  end
+
+  def encode(item = %Item{id: @payload}) do
+    {:ok, msg_payload} = Poison.encode(item.data)
+    payload_size = byte_size(msg_payload)
+    {:ok, <<item.id::size(8), payload_size::size(16)>> <> msg_payload}
+  end
+
+  def encode(item = %Item{id: @notification_identifier}) do
+    {:ok, <<item.id::size(8), 4::size(16), item.data::size(32)>>}
+  end
+
+  def encode(item = %Item{id: @expiration_date}) do
+    date = item.data
+    if date == nil do
+      date = 0
     end
+    {:ok, <<item.id::size(8), 4::size(16), date::size(32)>>}
+  end
+
+  def encode(item = %Item{id: @priority}) do
+    msg_priority = item.data
+    if msg_priority == nil do
+      msg_priority = @priority_immediately
+    end
+    {:ok, <<item.id::size(8), 1::size(16), msg_priority::size(32)>>}
   end
 end
