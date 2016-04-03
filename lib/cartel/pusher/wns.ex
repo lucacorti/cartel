3341f2cc1d1112cfeafeed50bd852ea0638012e2
Luca Corti <luca@fantacast.it>
2016-04-03 20:00:23 +0200

    Also defer wns login

--- lib/cartel/pusher/wns.ex
+++ lib/cartel/pusher/wns.ex
@@ -14,23 +14,33 @@ defmodule Cartel.Pusher.Wns do
   end
 
   def init(state) do
+    {:ok, %{conf: state, token: nil}}
+  end
+
+  defp login(id, conf) do
     query = [
       "grant_type": "client_credentials",
-      "client_id": state[:id],
-      "client_secret": state[:client_secret],
+      "client_id": id,
+      "client_secret": conf.client_secret,
       "scope": "notify.windows.com"
     ]
     res = HTTPotion.post(@wns_login_url, [query: query])
-    login_respond(res, state)
+    login_response(res)
   end
 
-  defp login_respond(res = %HTTPotion.Response{status_code: code}, state)
+  defp login_response(res = %HTTPotion.Response{status_code: code})
   when code >= 400 do
-    {:stop, res.status_code, state}
+    {:error, res.status_code}
   end
 
-  defp login_respond(res = %HTTPotion.Response{}, state) do
-    {:ok, [[token: Poison.decode(res.body)[:access_token]] | state]}
+  defp login_response(res = %HTTPotion.Response{}) do
+    {:ok, body} = Poison.decode(res.body)
+    {:ok, body[:access_token]}
+  end
+
+  def handle_call({:send, message}, from, state = %{token: nil}) do
+      {:ok, token} = login(state.id, state.conf)
+      handle_call({:send, message}, from, %{state | token: token})
   end
 
   def handle_call({:send, message}, _from, state) do
@@ -39,15 +49,15 @@ defmodule Cartel.Pusher.Wns do
     headers = ["Content-Type": "text/xml"]
     res = HTTPotion.post(@wns_login_url, [body: request, headers: headers,
                         query: query])
-    send_respond(res, state)
+    send_response(res, state)
   end
 
-  defp send_respond(res = %HTTPotion.Response{status_code: code}, state)
+  defp send_response(res = %HTTPotion.Response{status_code: code}, state)
   when code >= 400 do
     {:stop, res.code, state}
   end
 
-  defp send_respond(res = %HTTPotion.Response{}, state) do
+  defp send_response(res = %HTTPotion.Response{}, state) do
     {:reply, {:ok, res.status_code, Message.deserialize(res.body)}, state}
   end
 end
