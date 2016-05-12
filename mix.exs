939999a0b4e2e733039f560c0132209e13b9b4c4
Luca Corti <luca@fantacast.it>
2016-05-12 22:59:23 +0200

    Factor out package and doc information in defps

--- mix.exs
+++ mix.exs
@@ -6,40 +6,26 @@ defmodule Cartel.Mixfile do
      version: "0.1.0",
      elixir: "~> 1.2",
      description: "Multi platform, multi app push notifications",
-     package: [
-       maintainers: ["Luca Corti"],
-       licenses: ["MIT"],
-       links: %{ "GitHub": "https://github.com/lucacorti/cartel" }
-     ],
-     docs: [
-       main: "main",
-       extras: [
-         "docs/main.md",
-         "docs/usage.md"
-       ]
-     ],
+     package: package,
+     docs: docs,
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
     [applications: [:logger, :ssl, :poolboy, :poison, :httpotion, :chatterbox],
      mod: {Cartel, []}]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
+  defp package do
+    [
+      maintainers: ["Luca Corti"],
+      licenses: ["MIT"],
+      links: %{ "GitHub": "https://github.com/lucacorti/cartel" }
+    ]
+  end
+
   defp deps do
     [
       {:ex_doc, ">= 0.0.0", only: [:dev]},
@@ -50,6 +36,13 @@ defmodule Cartel.Mixfile do
       {:httpotion, "~> 2.2.0"},
       {:poolboy, "~> 1.5.1"},
       {:chatterbox, github: "joedevivo/chatterbox", branch: :master}
+    ]
+  end
+
+  defp docs do
+    [
+      main: "main",
+      extras: ["docs/main.md", "docs/usage.md"]
     ]
   end
 end
