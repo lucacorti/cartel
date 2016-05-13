b462ce06baa0da35ba2e75529d14cf4234b65584
Luca Corti <luca@fantacast.it>
2016-05-13 23:23:12 +0200

    Release 0.2.0

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Cartel.Mixfile do
 
   def project do
     [app: :cartel,
-     version: "0.1.0",
+     version: "0.2.0",
      elixir: "~> 1.2",
      description: "Multi platform, multi app push notifications",
      package: package,
