150adfd32e7f2937608a6373515116e1ed117e4f
Luca Corti <luca@fantacast.it>
2016-05-18 01:31:47 +0200

    Add Cartel.Message.Gcm docs

--- lib/cartel/message/gcm.ex
+++ lib/cartel/message/gcm.ex
@@ -1,6 +1,12 @@
 defmodule Cartel.Message.Gcm do
   @moduledoc """
   Google GCM message
+
+  - `to`: `String.t`, recipient registration token
+  - `data`: `Map`, the notification payload
+
+  For more details on the format see [Simple Downstream Messaging](https://developers.google.com/cloud-messaging/downstream)
+  section of the [Google Cloud Messaging Documentation](https://developers.google.com/cloud-messaging/)
   """
   @derive Poison.Encoder
 
