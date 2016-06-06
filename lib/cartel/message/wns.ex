427126b9e6aae8a5beb0355dff1b8b580d961dec
Luca Corti <luca.corti@wind.it>
2016-06-06 17:44:14 +0200

    Initial implementation of WNS

--- lib/cartel/message/wns.ex
+++ lib/cartel/message/wns.ex
@@ -5,15 +5,37 @@ defmodule Cartel.Message.Wns do
 
   @type t :: %__MODULE__{}
 
-  defstruct []
+  @type_toast "wns/toast"
+  def type_toast, do: @type_toast
+
+  @type_badge "wns/badge"
+  def type_badge, do: @type_badge
+
+  @type_tile "wns/tile"
+  def type_tile, do: @type_tile
+
+  @type_raw "wns/raw"
+  def type_raw, do: @type_raw
+
+  defstruct [channel: nil, type: @type_raw, cache_policy: false, tag: nil, ttl: 0,
+             suppress_popup: false, request_for_status: false, group: nil,
+             payload: ""]
+
+  def content_type(%__MODULE__{type: @type_raw}) do
+    "application/octet-stream"
+  end
+
+  def content_type(%__MODULE__{}) do
+    "text/xml"
+  end
 end
 
 defimpl Cartel.Message, for: Cartel.Message.Wns do
   def serialize(message) do
-    Poison.encode(message)
+    message.payload
   end
 
-  def update_token(message, _) do
-    message
+  def update_token(message, token) do
+    %{message | channel: token}
   end
 end
--- lib/cartel/pusher/wns.ex
+++ lib/cartel/pusher/wns.ex
@@ -10,12 +10,11 @@ defmodule Cartel.Pusher.Wns do
   alias HTTPotion.Response
 
   @wns_login_url "https://login.live.com/accesstoken.srf"
-  @wns_server_url "https://cloud.notify.windows.com"
 
   @doc """
   Starts the pusher
   """
-  @spec start_link(%{client_secret: String.t}) :: GenServer.on_start
+  @spec start_link(%{sid: String.t, secret: String.t}) :: GenServer.on_start
   def start_link(args), do: GenServer.start_link(__MODULE__, args)
 
   def init(conf = %{}), do: {:ok, %{conf: conf, token: nil}}
@@ -27,44 +26,64 @@ defmodule Cartel.Pusher.Wns do
   def push(pid, message), do: GenServer.call(pid, {:push, message})
 
   def handle_call({:push, message}, from, state = %{token: nil}) do
-      {:ok, token} = login(state.id, state.conf)
+      {:ok, token} = login(state.conf.sid, state.conf.secret)
       handle_call({:push, message}, from, %{state | token: token})
   end
 
   def handle_call({:push, message}, _from, state) do
-    {:ok, request} = Message.serialize(message)
-    query = ["token": state[:token]]
-    headers = ["Content-Type": "text/xml"]
-    res = HTTPotion.post(@wns_login_url, [body: request, headers: headers,
-                        query: query])
+    headers = [
+      "Content-Type": Wns.content_type(message),
+      "Authorization": "Bearer #{state.token}",
+      "X-WNS-Type": message.type
+    ]
+
+    if is_boolean(message.cache_policy) && message.cache_policy do
+      headers = ["X-WNS-Cache-Policy": "cache"] ++ headers
+    end
+
+    if String.valid?(message.tag) && String.length(message.tag) > 0 do
+      headers = ["X-WNS-Tag": message.tag] ++ headers
+    end
+
+    if String.valid?(message.group) && String.length(message.group) > 0 do
+      headers = ["X-WNS-Group": message.group] ++ headers
+    end
+
+    if is_integer(message.ttl) and message.ttl > 0 do
+      headers = ["X-WNS-TTL": message.ttl] ++ headers
+    end
+
+    if is_boolean(message.suppress_popup) && message.suppress_popup do
+      headers = ["X-WNS-SuppressPopup": "true"] ++ headers
+    end
+
+    if is_boolean(message.request_for_status) && message.request_for_status do
+      headers = ["X-WNS-RequestForStatus": "true"] ++ headers
+    end
+
+    body = Message.serialize(message)
+    res = HTTPotion.post(message.channel, [headers: headers, body: body])
     send_response(res, state)
   end
 
-  defp login(id, conf) do
-    query = [
-      "grant_type": "client_credentials",
-      "client_id": id,
-      "client_secret": conf.client_secret,
-      "scope": "notify.windows.com"
-    ]
-    res = HTTPotion.post(@wns_login_url, [query: query])
-    login_response(res)
+  defp send_response(%Response{status_code: code, headers: headers}, state)
+  when code >= 400 do
+    {:reply, {:error, headers.hdrs}, state}
   end
 
-  defp login_response(%Response{status_code: code}) when code >= 400 do
-    {:error, code}
+  defp send_response(%Response{headers: headers}, state) do
+    {:reply, {:ok, headers.hdrs}, state}
   end
 
-  defp login_response(%Response{body: body}) do
-    {:ok, body} = Poison.decode(body)
-    {:ok, body[:access_token]}
-  end
+  defp login(client_id, client_secret) do
+    gt = URI.encode_www_form("client_credentials")
+    sc = URI.encode_www_form("notify.windows.com")
+    cid = URI.encode_www_form(client_id)
+    cs = URI.encode_www_form(client_secret)
+    body = "grant_type=#{gt}&scope=#{sc}&client_id=#{cid}&client_secret=#{cs}"
+    headers = ["Content-Type": "application/x-www-form-urlencoded"]
 
-  defp send_response(%Response{status_code: code}, state) when code >= 400 do
-    {:stop, code, state}
-  end
-
-  defp send_response(%Response{status_code: code, body: body}, state) do
-    {:reply, {:ok, code, Poison.decode!(body)}, state}
+    res = HTTPotion.post(@wns_login_url, [headers: headers, body: body])
+    {:ok, Poison.decode!(res.body)["access_token"]}
   end
 end
