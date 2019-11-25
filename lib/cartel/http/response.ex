32a79ed1abb99cb9a37a0a10783f491a048812dc
Luca Corti <luca@fantacast.it>
2019-11-25 23:14:16 +0100

    Missing from previous

---
+++ lib/cartel/http/response.ex
@@ -0,0 +1,24 @@
+defmodule Cartel.HTTP.Response do
+  @moduledoc """
+  HTTP Response
+  """
+
+  alias Cartel.HTTP
+
+  @typedoc "HTTP response"
+  @type t :: %__MODULE__{
+          status: HTTP.status(),
+          headers: HTTP.headers(),
+          body: HTTP.body()
+        }
+  defstruct status: nil, headers: [], body: []
+
+  @spec status(t()) :: HTTP.status()
+  def status(%__MODULE__{status: status}), do: status
+
+  @spec headers(t()) :: HTTP.headers()
+  def headers(%__MODULE__{headers: headers}), do: headers
+
+  @spec body(t()) :: HTTP.body()
+  def body(%__MODULE__{body: body}), do: body
+end
---
+++ lib/cartel/pusher/apns.ex
@@ -0,0 +1,111 @@
+defmodule Cartel.Pusher.Apns do
+  @moduledoc """
+  Apple APNS Provider API worker
+  """
+
+  use GenServer
+  use Cartel.Pusher, message_module: Cartel.Message.Apns
+
+  alias Cartel.HTTP
+  alias Cartel.Message.Apns
+  alias HTTP.{Request, Response}
+
+  @production_url "https://api.push.apple.com"
+  @sandbox_url "https://api.development.push.apple.com"
+
+  @doc """
+  Starts the pusher
+  """
+  @spec start_link(%{
+          env: :production | :sandbox,
+          cert: String.t(),
+          key: String.t(),
+          cacert: String.t()
+        }) :: GenServer.on_start()
+  def start_link(args), do: GenServer.start_link(__MODULE__, args, [])
+
+  def init(conf), do: {:ok, %{conf: conf, headers: nil, pid: nil}}
+
+  def handle_push(process, message, payload) do
+    GenServer.call(process, {:push, message, payload})
+  end
+
+  def handle_call({:push, message, payload}, from, %{pid: nil, headers: nil, conf: conf} = state) do
+    {:ok, pid, url} = connect(conf)
+    handle_call({:push, message, payload}, from, %{state | pid: pid, url: url})
+  end
+
+  def handle_call({:push, message, payload}, _from, %{url: url} = state) do
+    headers = message_headers(message)
+
+    request =
+      url
+      |> Request.new("POST")
+      |> Request.set_body(payload)
+      |> Request.set_headers(headers)
+      |> Request.put_header({"accept", "application/json"})
+      |> Request.put_header({"accept-encoding", "gzip, deflate"})
+
+    case HTTP.request(%HTTP{}, request) do
+      {:ok, _, %Response{status: code}} when code >= 400 ->
+        {:reply, {:error, :unauthorized}, state}
+
+      {:ok, _, %Response{body: body}} ->
+        case Jason.decode!(body) do
+          %{"results" => [%{"message_id" => _id}]} ->
+            {:reply, :ok, state}
+
+          %{"results" => [%{"error" => error}]} ->
+            {:reply, {:error, error}, state}
+        end
+
+      {:error, reason} ->
+        {:error, reason}
+    end
+  end
+
+  defp connect(%{env: :sandbox, cert: cert, key: key, cacert: cacert}) do
+    with {:ok, pid} <- HTTP.connect(@sandbox_url, certfile: cert, keyfile: key, cacertfile: cacert),
+         do: {:ok, pid, @sandbox_url}
+  end
+
+  defp connect(%{env: :production, cert: cert, key: key, cacert: cacert}) do
+    with {:ok, pid} = HTTP.connect(@production_url, certfile: cert, keyfile: key, cacertfile: cacert),
+      do: {:ok, pid, @production_url}
+  end
+
+  defp message_headers(message) do
+    []
+    |> add_message_priority_header(message)
+    |> add_message_expiration_header(message)
+    |> add_message_id_header(message)
+    |> add_message_topic_header(message)
+    |> add_message_path_header(message)
+  end
+
+  defp add_message_priority_header(headers, %Apns{priority: priority})
+       when is_integer(priority) do
+    [{":apns-priority", "#{priority}"} | headers]
+  end
+
+  defp add_message_expiration_header(headers, %Apns{expiration: expiration})
+       when is_integer(expiration) do
+    [{":apns-expiration", "#{expiration}"} | headers]
+  end
+
+  defp add_message_id_header(headers, %Apns{id: id}) when is_binary(id) do
+    [{":apns-id", "#{id}"} | headers]
+  end
+
+  defp add_message_id_header(headers, _), do: headers
+
+  defp add_message_topic_header(headers, %Apns{topic: topic}) when is_binary(topic) do
+    [{":apns-topic", "#{topic}"} | headers]
+  end
+
+  defp add_message_topic_header(headers, _), do: headers
+
+  defp add_message_path_header(headers, %Apns{token: token}) when is_binary(token) do
+    [{":path", "/3/device/#{token}"} | headers]
+  end
+end
